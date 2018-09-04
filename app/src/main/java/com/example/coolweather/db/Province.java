897ca2c2f5867719520a78281935deae6c31cec1
董海涛 <haitao.dong@w-oasis.com>
2018-09-04 18:12:02 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/coolweather/db/Province.java
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -1,8 +1,8 @@
 package com.example.coolweather.db;
 
-import org.litepal.crud.LitePalSupport;
+import org.litepal.crud.DataSupport;
 
-public class Province extends LitePalSupport {
+public class Province extends DataSupport {
 
     private int id;
     private String provinceName;    // 省的名字
